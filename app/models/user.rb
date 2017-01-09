5d6651bae1034a6f7e6aead2b95ab56b22f899b2
Tomás de Lima <tomasdelima@gmail.com>
2017-01-09 16:41:25 -0200

    Adds devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable #, :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :songs
+  validates :name, presence: true
 end
