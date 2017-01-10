2ec14811102189d96e7ace04cf01ca907ddf9419
Tomás de Lima <tomasdelima@gmail.com>
2017-01-09 22:30:24 -0200

    Ads the songs controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,18 @@
+class SongsController < ApplicationController
+  # before_action :authenticate_user!, only: :create
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    songs = Song.all.where("updated_at > #{params[:last_updated_at] || Date.new(2000, 1, 1)}")
+    render json: {data: songs.to_json, time: Time.now.utc}
+  end
+
+  def create
+    song = Song.new(title: params[:title], lyrics: params[:lyrics], chords: params[:chords], author: current_user)
+    if song.save
+      render json: true
+    else
+      render json: {errors: song.errors}
+    end
+  end
+end
